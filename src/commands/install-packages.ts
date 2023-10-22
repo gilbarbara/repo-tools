6ab413427d37b06d3944d015768a807680e17029
Gil Barbara <gilbarbara@gmail.com>
2023-10-22 20:34:45 -0300

    Implement detect-package-manager

--- src/commands/install-packages.ts
+++ src/commands/install-packages.ts
@@ -1,21 +1,33 @@
 import { exec } from 'child_process';
 
+import { detect } from 'detect-package-manager';
 import { green, yellow } from 'kolorist';
 
 import { run } from '../helpers';
 
 export const command = 'install-packages';
-export const describe = 'Run `npm install` if package.json has changed';
-export const handler = (): Promise<void> =>
-  run('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD')
-    .then(({ stdout }) => {
-      if (stdout.match('package.json')) {
-        console.log(yellow('▼ package.json is modified. Running `npm install`...'));
-        exec('npm install').stdout?.pipe(process.stdout);
-      } else {
-        console.log(green('✔ Nothing to update'));
-      }
-    })
-    .catch(error => {
-      throw new Error(error);
-    });
+export const describe = 'Run `npm/yarn/pnpm/bun install` if package.json has changed';
+
+const install = (pm: string = 'npm') => {
+  console.log(yellow(`▼ package.json is modified. Running "${pm} install"...`));
+
+  exec(`${pm} install`).stdout?.pipe(process.stdout);
+};
+
+export const handler = async (): Promise<void> => {
+  const { stdout } = await run('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD');
+
+  if (stdout.match('package.json')) {
+    try {
+      const pm = await detect();
+
+      install(pm);
+    } catch {
+      console.warn('Could not detect package manager. Trying npm...');
+
+      install();
+    }
+  } else {
+    console.log(green('✔ Nothing to update'));
+  }
+};
